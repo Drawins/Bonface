e9d8306115beec49adb05e333cd9458e5c79822d
Darwins <darwinsomondi5@gmail.com>
2023-10-22 11:54:21 +0300

    version2

--- work.dart
+++ work.dart
@@ -1,3 +1,34 @@
+// void main(){
+//  String sen1 = 'How are you doing.';
+//  String sen2 ='It has been long since we first met.';
+//  if (sen1.length>sen2.length) {
+//    print(sen1);
+//  } else{
+//   print(sen2);
+//  }
+// }
+// import 'dart:io';
+
+// void main (){
+//   print('Enter your name:');
+//   var name = stdin.readLineSync();
+//   String name1=name.toString();
+//   if (name1 =='Bob') {
+//     print('Hello $name1');
+//   } else if(name1 =='Alice'){
+//     print('Hello $name1');
+//   }else{
+//     print('Hello');
+//   }
+//   }
+import 'dart:io';
+
 void main(){
-  print('hello world');
+  print('Enter your number:');
+  var num = stdin.readByteSync();
+  int text = 0;
+  for (var i = 0; i < num; i++) {
+    text += i;
+  }
+  print(text);
 }
