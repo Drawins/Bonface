98b6ff4a4c966dcabda0bc5226ec189124efb732
Darwins <darwinsomondi5@gmail.com>
2023-10-23 15:36:13 +0300

    practice

--- work.dart
+++ work.dart
@@ -1,34 +1,9 @@
-// void main(){
-//  String sen1 = 'How are you doing.';
-//  String sen2 ='It has been long since we first met.';
-//  if (sen1.length>sen2.length) {
-//    print(sen1);
-//  } else{
-//   print(sen2);
-//  }
-// }
-// import 'dart:io';
+import 'dart:io';
 
-// void main (){
-//   print('Enter your name:');
-//   var name = stdin.readLineSync();
-//   String name1=name.toString();
-//   if (name1 =='Bob') {
-//     print('Hello $name1');
-//   } else if(name1 =='Alice'){
-//     print('Hello $name1');
-//   }else{
-//     print('Hello');
-//   }
-//   }
-// import 'dart:io';
-
-// void main(){
-//   print('Enter your number:');
-//   var num = stdin.readByteSync();
-//   int text = 0;
-//   for (var i = 0; i < num; i++) {
-//     text += i;
-//   }
-//   print(text);
-//}
+void main(){
+  print('Enter your name');
+  var name = stdin.readLineSync();
+  print('Enter your age:');
+  int age = stdin.readByteSync();
+  print('hello $name your are $age years old');
+}
