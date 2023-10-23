76c24084b1f9cefeabcecf15ef6398f6c626e3be
Darwins <darwinsomondi5@gmail.com>
2023-10-23 15:47:15 +0300

    practice2

--- work.dart
+++ work.dart
@@ -1,9 +1,16 @@
 import 'dart:io';
 
 void main(){
-  print('Enter your name');
-  var name = stdin.readLineSync();
-  print('Enter your age:');
-  int age = stdin.readByteSync();
-  print('hello $name your are $age years old');
+  // print('Enter your name');
+  // var name = stdin.readLineSync();
+  // print('Enter your age:');
+  // int age = stdin.readByteSync();
+  // print('hello $name your are $age years old');
+   print('Enter your number:');
+  int num = stdin.readByteSync();
+  if (num%2==0) {
+    print('$num is an even number.');
+  } else{
+    print('$num is an odd number.');
+  }
 }
